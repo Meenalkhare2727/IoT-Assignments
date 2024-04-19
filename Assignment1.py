b2ac98a002b6828ada6076b0031f6e5f452d6999
Meenalkhare2727 <meenalkhare027@gmail.com>
2024-04-19 21:30:54 +0530

    IoT_assignments

---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+
+#find area of reactangle
+
+
+le = float(input("Enter length of reactangle:"))
+br = float(input("Enter breadth of reactangle:"))
+
+area = le*br
+perimeter= 2*(le+br)
+
+print(f"Area of rectangle ={area}")
+print(f"Perimeter of recatngle={perimeter}") 
+
+
+
+
+
+
+
+
